3556cd5fe5341e7b6a91eed015ac8b53e9bd0219
FatmaMohamedTaha <12p6026@eng.asu.edu.eg>
2017-03-06 16:43:15 +0200

    First Commit
    assignment1

---
+++ app/src/main/java/com/example/fatma/mobileassignment/Login.java
@@ -0,0 +1,45 @@
+package com.example.fatma.mobileassignment;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    Button signIn;
+    EditText username,password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        signIn = (Button)findViewById(R.id.signInbtn);
+        username=(EditText)findViewById(R.id.UsernameLoginET);
+        password=(EditText)findViewById(R.id.PasswordLoginET);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("") || password.getText().toString().equals("") )
+                {
+                    Toast.makeText(Login.this,"Please fill all the fields",Toast.LENGTH_SHORT).show();
+                }
+
+
+                Toast.makeText(Login.this,"Sign in button",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/fatma/mobileassignment/Signup.java
@@ -0,0 +1,71 @@
+package com.example.fatma.mobileassignment;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Signup extends AppCompatActivity {
+
+    Button signup;
+    EditText username,email,password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        signup = (Button)findViewById(R.id.registerBtn);
+        username=(EditText)findViewById(R.id.UsernameET);
+        email=(EditText)findViewById(R.id.EmailET);
+        password=(EditText)findViewById(R.id.PasswordET);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname= username.getText().toString();
+                String uemail= email.getText().toString();
+                String upass=password.getText().toString();
+                if(uname.equals("") || uemail.equals("") || upass.equals(""))
+                {
+                    Toast.makeText(Signup.this,"Please fill all the fields",Toast.LENGTH_SHORT).show();
+                }
+
+               else
+                    Toast.makeText(Signup.this,"Sign up button",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_signup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
